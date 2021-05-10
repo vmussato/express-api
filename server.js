825499817aba3fbc01903582d744ec2373d42230
Vinicius Mussato <vinicius.mussato@gmail.com>
2021-05-10 20:27:25 -0300

    changing value of return

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-app.use("/", (req, res) => res.send('Olá Mundo'))
+app.use("/", (req, res) => res.json({imgSrc:'http://www.fillmurray.com/400/500'}))
 
 app.listen(3001, function () {
   console.log('listening on port 3001')
