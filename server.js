217adc109b52e244d4ab21d60a119cac1f211f12
Vinícius Mussato <vinicius.mussato@gmail.com>
2023-11-03 10:47:58 -0300

    teste commit

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express();
 app.use("/", (req, res) => res.json({imgSrc:'http://www.fillmurray.com/400/500'}))
 
 app.listen(3001, function () {
-  console.log('listening on port 3001')
+  console.log('listening on port 3001, hello')
 })
